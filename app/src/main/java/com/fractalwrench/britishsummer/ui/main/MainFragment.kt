9153cdafdc035d86ac6e57c5a99aaba3bdccf724
fractalwrench <fractalwrench@gmail.com>
2018-06-23 16:32:02 +0100

    pass ktlint

--- app/src/main/java/com/fractalwrench/britishsummer/ui/main/MainFragment.kt
+++ app/src/main/java/com/fractalwrench/britishsummer/ui/main/MainFragment.kt
@@ -7,8 +7,6 @@ import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.fractalwrench.britishsummer.CurrentWeatherRepository
 import com.fractalwrench.britishsummer.R
 import com.fractalwrench.britishsummer.hideKeyboard
 import com.jakewharton.rxbinding2.widget.RxTextView
@@ -20,7 +18,6 @@ import kotlinx.android.synthetic.main.main_fragment.temp_desc
 import kotlinx.android.synthetic.main.main_fragment.weather_desc
 import kotlinx.android.synthetic.main.main_fragment.wind_desc
 import org.koin.android.architecture.ext.android.viewModel
-import org.koin.android.ext.android.inject
 import java.util.Date
 
 class MainFragment : Fragment() {
